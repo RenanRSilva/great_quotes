8ba7e2d26767091003a5863ace1e900ec1ec0e1b
RenanRSilva <renanrambuls@gmail.com>
2022-03-23 11:39:27 -0300

    main-layout

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -5,7 +5,7 @@ import classes from './MainNavigation.module.css'
 const MainNavigation = () => {
   return <header className={classes.header}>
     <div className={classes.logo}>Great Quotes</div>
-    <nav className='classes.nav'>
+    <nav className={classes.nav}>
       <ul>
         <li>
           <NavLink to='/quotes' activeClassName={classes.active}>
@@ -13,7 +13,7 @@ const MainNavigation = () => {
           </NavLink>
         </li>
         <li>
-          <NavLink to='/new-quotes' activeClassName={classes.active}>
+          <NavLink to='/new-quote' activeClassName={classes.active}>
             Add a Quotes
           </NavLink>
         </li>
