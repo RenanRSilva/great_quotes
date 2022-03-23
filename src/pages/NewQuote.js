8f642cbddd556fc4a1ddafcacc516891b7b74e1d
RenanRSilva <renanrambuls@gmail.com>
2022-03-23 14:25:29 -0300

    quote page

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,6 +1,21 @@
-const NewQuote = () => {
-  return <h1>New Quote Page</h1>
+import { useHistory } from 'react-router-dom';
 
-}
+import QuoteForm from '../components/quotes/QuoteForm'
+
+const NewQuote = () => {
+  const history = useHistory();
+
+  const addQuoteHandler = quoteData => {
+    console.log(quoteData);
+
+    history.push('/quotes');
+  };
+
+  return (
+    <QuoteForm
+      onAddQuote={addQuoteHandler}
+    />
+  )
+};
 
 export default NewQuote
---
+++ src/pages/NotFound.js
@@ -0,0 +1,7 @@
+const NotFound = () => {
+  <div className="centered">
+    <p>Page Not Found</p>
+  </div>
+};
+
+export default NotFound;
